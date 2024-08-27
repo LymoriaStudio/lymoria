896e623478801fb76f103a0fdfbab3aa3ede5a79
Izabela <izabelaizidoriobr@gmail.com>
2024-08-27 12:38:04 -0300

    sobre nos

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+/*burger menu*/
 function toggleMenu() {
   const menu = document.querySelector(".header-menu");
   menu.classList.toggle("active");
+}
+
+/*FAQ*/
+function toggleFaq(element) {
+  var faqItem = element.parentElement;
+  faqItem.classList.toggle("active");
+
+  var synopsis = faqItem.querySelector(".faq-synopsis");
+  var answer = faqItem.querySelector(".faq-answer");
+
+  if (faqItem.classList.contains("active")) {
+    synopsis.style.display = "none";
+    answer.style.display = "block";
+  } else {
+    synopsis.style.display = "block";
+    answer.style.display = "none";
+  }
 }
